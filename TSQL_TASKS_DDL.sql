692c2bdc843d0f2a9d10e853875f8d00d1925844
Callum Hopkins <103919468@student.swin.edu.au>
2022-10-10 15:17:32 +1100

    Q8

--- TSQL_TASKS_DDL.sql
+++ TSQL_TASKS_DDL.sql
@@ -173,10 +173,12 @@ BEGIN
     ELSE
       UPDATE CUSTOMER9468
       SET Sales_YTD = Sales_YTD + @pamt
-      WHERE CUSTID = @pcustid;
+      WHERE CUSTID = @pcustid
+        IF @@ROWCOUNT = 0
+          THROW 50070, 'Customer ID not found', 1;
   END TRY
   BEGIN CATCH
-    IF ERROR_NUMBER() = 547
+    IF ERROR_NUMBER() = 50070
       THROW 50070, 'Customer ID not found', 1;
     ELSE IF ERROR_NUMBER() = 50080
       THROW 50080, 'Amount out of range', 1;
@@ -205,10 +207,12 @@ BEGIN
     ELSE
       UPDATE PRODUCT9468
       SET Sales_YTD = Sales_YTD + @pamt
-      WHERE PRODID = @pprodid;
+      WHERE PRODID = @pprodid
+        IF @@ROWCOUNT = 0
+          THROW 50100, 'Product ID not found', 1;
   END TRY
   BEGIN CATCH
-    IF ERROR_NUMBER() = 547
+    IF ERROR_NUMBER() = 50100
       THROW 50100, 'Product ID not found', 1;
     ELSE IF ERROR_NUMBER() = 50110
       THROW 50110, 'Amount out of range', 1;
@@ -223,7 +227,90 @@ EXEC UPD_PROD_SALESYTD @pprodid = 999999999, @pamt = 100.00;
 GO
 EXEC UPD_PROD_SALESYTD @pprodid = 1, @pamt = -1000.00;
 GO
-EXEC UPD_PROD_SALESYTD @pprodid = 1, @pamt = 100.00;
+EXEC UPD_PROD_SALESYTD @pprodid = 1000, @pamt = 100.00;
 GO
 
-SELECT * FROM PRODUCT9468;
+-- 6
+DROP PROCEDURE IF EXISTS SUM_CUSTOMER_SALESYTD;
+GO
+CREATE PROCEDURE SUM_CUSTOMER_SALESYTD AS
+BEGIN
+  BEGIN TRY
+    SELECT SUM(Sales_YTD) AS Total_Sales
+    FROM CUSTOMER9468;
+  END TRY
+  BEGIN CATCH
+    DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+    THROW 50000, @ERRORMESSAGE, 1;
+  END CATCH
+END
+
+EXEC SUM_CUSTOMER_SALESYTD;
+GO
+
+
+
+-- 7
+DROP PROCEDURE IF EXISTS GET_ALL_PRODUCTS;
+GO
+CREATE PROCEDURE GET_ALL_PRODUCTS @POUTCUR CURSOR VARYING OUTPUT AS
+BEGIN
+  BEGIN TRY
+    SET @POUTCUR = CURSOR FOR
+    SELECT *
+    FROM PRODUCT9468;
+
+    OPEN @POUTCUR;
+  END TRY
+  BEGIN CATCH
+    DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+    THROW 50000, @ERRORMESSAGE, 1;
+  END CATCH
+END
+GO
+
+BEGIN
+DECLARE @outProd AS CURSOR, @prodID INT, @prodName NVARCHAR(100), @sellingPrice MONEY, @salesYTD MONEY;
+EXEC GET_ALL_PRODUCTS @POUTCUR = @outProd OUTPUT;
+
+FETCH NEXT FROM @outProd INTO @prodID, @prodName, @sellingPrice, @salesYTD;
+
+WHILE @@FETCH_STATUS = 0
+  BEGIN
+    PRINT CONCAT ('Product ID: ', @prodID, ', Product Name: ', @prodName, ', Selling Price: ', @sellingPrice, ', Sales YTD: ', @salesYTD);
+    FETCH NEXT FROM @outProd INTO @prodID, @prodName, @sellingPrice, @salesYTD;
+  END
+
+CLOSE @outProd;
+DEALLOCATE @outProd;
+END
+GO
+
+
+-- 8
+DROP PROCEDURE IF EXISTS ADD_SALE;
+GO
+CREATE PROCEDURE ADD_SALE @pcustid INT, @pprodid INT, @pqty INT, @pdate DATE AS
+BEGIN
+  BEGIN TRY
+    DECLARE @pCustStatus NVARCHAR(7)
+
+    IF @pqty < 1 OR @pqty > 999
+      THROW 50230, 'Sale quantity outside valid range', 1;
+    -- ELSE IF 
+
+    INSERT INTO SALE9468 (SALEID, CUSTID, PRODID, QTY, SALEDATE)
+    VALUES (NEXT VALUE FOR Sale_SEQ, @pcustid, @pprodid, @pqty, @pdate);
+  END TRY
+  BEGIN CATCH
+    IF ERROR_NUMBER() = 50230
+      THROW 50230, 'Sale quantity outside valid range', 1;
+    ELSE
+      DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+      THROW 50000, @ERRORMESSAGE, 1;
+  END CATCH
+END
+    
+EXEC ADD_SALE @pcustid = 1, @pprodid = 1, @pqty = 1, @pdate = '2019-01-01';
+
+SELECT * FROM sys.sequences WHERE name = 'SALE_SEQ';
